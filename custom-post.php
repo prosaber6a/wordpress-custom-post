00b3853cfa2a27ea0c486f754a61072092f497a8
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-16 20:21:34 +0600

    Custom post added

--- custom-post.php
+++ custom-post.php
@@ -11,11 +11,46 @@
  * Domain Path: /languages/
  */
 
-function custom_post_load_text_domain () {
-	load_plugin_textdomain('custom-post', false, plugin_dir_url(__FILE__) . '/languages');
+function custom_post_load_text_domain() {
+	load_plugin_textdomain( 'custom-post', false, plugin_dir_url( __FILE__ ) . '/languages' );
 }
-add_action('plugins_loaded', 'custom_post_load_text_domain');
+
+add_action( 'plugins_loaded', 'custom_post_load_text_domain' );
 
 
+function custom_post_register() {
+	$labels = array(
+		'name'          => __( 'Recipes', 'custom-post' ),
+		'singular_name' => __( 'Recipe', 'custom-post' ),
+		'all_items'     => __( 'My Recipes', 'custom-post' ),
+		'add_new'       => __( 'New Recipe', 'custom-post' )
+	);
 
+	$args = array(
+		'label'               => __( 'Recipes', 'custom-post' ),
+		'labels'              => $labels,
+		'description'         => '',
+		'public'              => true,
+		'publicly_queryable'  => true,
+		'show_ui'             => true,
+		'show_in_rest'        => false,
+		'rest_base'           => '',
+		'has_archive'         => 'recipes',
+		'show_in_menu'        => true,
+		'show_in_nav_menu'    => true,
+		'exclude_from_search' => false,
+		'capability_type'     => 'post',
+		'map_meta_cap'        => true,
+		'hierarchical'        => false,
+		'query_var'           => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-images-alt',
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+		'taxonomies'          => array( 'category' )
+	);
+
+	register_post_type( 'recipe', $args );
+}
+
+add_action( 'init', 'custom_post_register' );
 
